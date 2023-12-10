bd83e8cca9ffb5785ca3d820acf6a40a8c63c886
grapefruit13 <liam.heo11@gmail.com>
2023-12-10 12:37:27 +0900

    build: eslint, prettier 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, node: true },
+  env: { browser: true, es2021: true, node: true },
   extends: [
     'airbnb',
     'plugin:prettier/recommended',
