04509edee8b8d5c7e79a735b88ec31f803c3ad13
KimByeongHwa <iambyeonghwa@gmail.com>
2023-12-12 21:50:02 +0900

    feat: Button/Floating 컴포넌트 UI 완성

---
+++ src/components/button/ButtonFloating.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.div`
+  cursor: pointer;
+  // layout
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20.8rem;
+  height: 5.4rem;
+  padding: 1.2rem 2.4rem;
+  gap: 8px;
+  flex-shrink: 0;
+  border-radius: 200px;
+  background: var(--brown-40, #542f1a);
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  // font
+  color: var(--grayscale-10, #fff);
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 2.5rem;
+`;
+
+function ButtonFloating({ children }) {
+  return <StyledButton>{children}</StyledButton>;
+}
+
+export default ButtonFloating;
