d84f32c84c61006542dfa034e6e9823969099842
grapefruit13 <liam.heo11@gmail.com>
2023-12-12 20:15:01 +0900

    style: index.html에 Pretendard 폰트 link하는 코드 추가, GlobalStyle Pretendard에 쌍따옴표 추가

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -27,7 +27,7 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
     margin: 0;
-    font-family: Pretendard, "Noto Sans KR", sans-serif;
+    font-family: "Pretendard", "Noto Sans KR", sans-serif;
   };
 
   html,
