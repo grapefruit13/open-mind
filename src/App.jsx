fac844a9759070aa75c91db8987825baacede494
caseV12 <caseVDev@gmail.com>
2023-12-13 17:08:27 +0900

    feat: Input컴포넌트 InputField.jsx, InputTextarea.jsx 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './GlobalStyle';
 import theme from './theme';
+import InputField from './components/InputField';
+import InputTextarea from './components/InputTextarea';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      App
+      <InputTextarea />
+      <InputField />
     </ThemeProvider>
   );
 }
