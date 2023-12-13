e40ef39ce347d6770a1b302aac5976dce5c9583f
grapefruit13 <liam.heo11@gmail.com>
2023-12-13 17:17:31 +0900

    fix: App.jsx 초기로 되돌리기

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './GlobalStyle';
 import theme from './theme';
-import InputField from './components/InputField';
-import InputTextarea from './components/InputTextarea';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <InputTextarea />
-      <InputField />
     </ThemeProvider>
   );
 }
