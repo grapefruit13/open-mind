4b1c7aefd32b3eeb4a7b5b2558924e1763dfbab0
KimByeongHwa <iambyeonghwa@gmail.com>
2023-12-13 16:01:30 +0900

    feat: button 관련 컴포넌트 UI 완성

--- src/assets/svgComponents/ArrowRight.jsx
+++ src/assets/svgComponents/ArrowRight.jsx
@@ -2,8 +2,8 @@ function ArrowRight() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="18"
+      height="18"
       viewBox="0 0 24 24"
       fill="currentColor"
     >
--- src/components/button/ButtonBox.jsx
+++ src/components/button/ButtonBox.jsx
@@ -23,34 +23,66 @@ const StyledButton = styled.button`
   line-height: 2.2rem;
 
   &:hover {
-    gap: 1rem;
-    border: 2px solid var(--brown-50, #341909);
     background: var(--brown-40, #542f1a);
-    /* box-shadow: 0 0 0 2px var(--brown-50, #341909); */ // border-box 효과 주고싶을 때
+    box-shadow: 0 0 0 2px var(--brown-50, #341909);
   }
 
   &:active {
-    gap: 1rem;
     background: var(--brown-50, #341909);
   }
 
-  // inactive 상태 - 협의 point: 배경색 'figma에 적혀있는 절댓값' vs %
-  ${props =>
-    props.disabled &&
-    css`
-      background: var(--brown-30, #c7bbb5);
-    `}
+  &:disabled {
+    opacity: 0.5;
+    box-shadow: none;
+  }
 
   &:disabled:hover,
   &:disabled:active {
-    gap: 0.8rem;
-    border: none;
-    background: var(--brown-30, #c7bbb5);
+    background: var(--brown-40, #542f1a);
   }
+
+  ${props =>
+    props.outline &&
+    css`
+      border: 1px solid var(--brown-40, #542f1a);
+      background: var(--brown-10, #f5f1ee);
+      color: var(--brown-40, #542f1a);
+
+      &:hover {
+        box-shadow: 0 0 0 2px var(--brown-50, #341909);
+        background: var(--brown-10, #f5f1ee);
+      }
+
+      &:active {
+        box-shadow: 0 0 0 2px var(--brown-40, #542f1a);
+        background: var(--brown-20, #e4d5c9);
+      }
+
+      &:disabled:hover,
+      &:disabled:active {
+        box-shadow: none;
+        background: var(--brown-10, #f5f1ee);
+      }
+    `}
+
+  ${props =>
+    props.small &&
+    css`
+      padding: 8px 12px;
+      gap: 4px;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 18px;
+    `}
 `;
 
-function ButtonBox({ children, isDisabled }) {
-  return <StyledButton disabled={isDisabled}>{children}</StyledButton>;
+function ButtonBox({ children, disabled, outline, small }) {
+  return (
+    <StyledButton disabled={disabled} outline={outline} small={small}>
+      {children}
+    </StyledButton>
+  );
 }
 
 export default ButtonBox;
