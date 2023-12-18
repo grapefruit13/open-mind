08d0f3fdc69c8cabf36decc623cd58952a997dfa
grapefruit13 <liam.heo11@gmail.com>
2023-12-18 21:28:46 +0900

    feat: 답변 달기/수정, 질문 삭제/거절 시 필요한 axios 함수 구현

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,9 +3,10 @@ import { BASE_URL } from '../constants/apiUrl';
 
 axios.defaults.baseURL = BASE_URL;
 
-export default async function getData(url) {
+// 유저 조회
+export default async function getUserData(url, subjectId) {
   try {
-    const response = await axios.get(`${url}`);
+    const response = await axios.get(`${url}${subjectId}`);
     return response.data;
   } catch (e) {
     throw Error(`getData에서 ${e} 발생`);
@@ -32,5 +33,92 @@ export async function getQuestionsData(url) {
     return response.data;
   } catch (e) {
     throw Error(`getQuestions에서 ${e} 발생`);
+  }
+}
+
+// 답변 하나 조회 (답변 추가,수정,삭제,거절 이후에 바로 실행)
+export async function getAnswer(url, answerId) {
+  try {
+    const response = await axios.get(`${url}${answerId}/`);
+    return response.data;
+  } catch (e) {
+    throw Error(`getAnswer ${e} 발생`);
+  }
+}
+
+// 답변 추가
+export async function postAnswer(url, questionId, content, isRejected) {
+  try {
+    const response = await axios.post(`${url}${questionId}/answers/`, {
+      content,
+      isRejected,
+    });
+    return response.data;
+  } catch (e) {
+    throw Error(`postAnswer ${e} 발생`);
+  }
+}
+
+// 답변 수정
+export async function putAnswer(url, answerId, content, isRejected) {
+  try {
+    const response = await axios.put(`${url}${answerId}/`, {
+      content,
+      isRejected,
+    });
+    return response.data;
+  } catch (e) {
+    throw Error(`putAnswer ${e} 발생`);
+  }
+}
+
+// 질문 삭제
+export async function deleteQuestion(url, questionId) {
+  try {
+    const response = await axios.delete(`${url}${questionId}/`);
+    return response.data;
+  } catch (e) {
+    throw Error(`putAnswer ${e} 발생`);
+  }
+}
+
+// 질문 전체 삭제
+export async function deleteAllQuestion(url, questionsIds) {
+  try {
+    const deleteRequests = questionsIds.map(deleteId => {
+      return axios.delete(`${url}${deleteId}/`);
+    });
+    console.log('전체삭제 api', deleteRequests);
+
+    const responses = await Promise.all(deleteRequests);
+
+    responses.forEach(response => {
+      console.log('DELETE 응답 데이터:', response.data);
+    });
+
+    console.log('전체 삭제 api 종료');
+  } catch (e) {
+    throw Error(`putAnswer ${e} 발생`);
+  }
+}
+
+// 질문 추가
+export async function postQuestion(subjectId, content) {
+  try {
+    axios.post(`subjects/${subjectId}/questions/`, {
+      content,
+    });
+  } catch (e) {
+    throw Error(`postQuestion ${e} 발생`);
+  }
+}
+
+// 대상 질문 목록 조회
+export async function getQuestions(subjectId) {
+  try {
+    const response = await axios.get(`subjects/${subjectId}/questions/`);
+    return response.data;
+  } catch (e) {
+    throw Error(`getQuestions ${e} 발생`);
   }
 }
