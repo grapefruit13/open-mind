e859e9052e9bdd0ede084b66084ecd81a39c3e6a
grapefruit13 <liam.heo11@gmail.com>
2023-12-18 21:11:43 +0900

    refactor: AnswerRejected 사용할 때 msg props로 주는 부분 삭제

--- src/components/common/feedCard/AnswerSection.jsx
+++ src/components/common/feedCard/AnswerSection.jsx
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
-import { useState } from 'react';
+import { useContext } from 'react';
 import UserProfileImg from '../userInfo/UserProfileImg';
 import UserName from '../userInfo/UserName';
 import DatesAgo from './DatesAgo';
 import AnswerContent from './AnswerContent';
-// import AnswerRejected from './AnswerRejected';
 import InputTextarea from '../InputTextarea';
 import ButtonBox from '../button/ButtonBox';
+import { DropdownContext } from '../../../utils/context';
+import AnswerRejected from './AnswerRejected';
 
 const Container = styled.div`
   display: flex;
@@ -30,14 +31,22 @@ const FlexRow = styled.div`
 `;
 
 // user 데이터를 끌고와야 함. 이 때, props drilling이 심하므로 contextAPI 도입 논의 필요
-export default function AnswerSection({ answer, user, path }) {
-  const [inputTextarea, setInputTextarea] = useState({
-    value: '',
-    hasValue: false,
-  });
+export default function AnswerSection({
+  answer,
+  user,
+  path,
+  editMode,
+  onClickComplete,
+}) {
+  const { inputTextarea, isCompleted } = useContext(DropdownContext);
 
-  const onChangeInput = state => {
-    setInputTextarea(prev => ({ ...prev, ...state }));
+  // 답변 완료 버튼 누르면 clickedBtns에 answered: true 넣어줌
+  const handleClickButtonBox = state => {
+    if (editMode) {
+      onClickComplete({ editCompleted: state });
+    } else {
+      onClickComplete({ answerCompleted: state });
+    }
   };
 
   return (
@@ -52,26 +61,41 @@ export default function AnswerSection({ answer, user, path }) {
           <UserName userName={user.name} size="1.8rem" />
           {!path && <DatesAgo text="2주전" />}
         </FlexRow>
-        {!answer && path === 'answer' && (
+        {!answer && path === 'answer' && !isCompleted.answerCompleted && (
           <>
-            <InputTextarea
-              type="답변"
-              height="18.6rem"
-              marginbottom="0.8rem"
-              onChangeInput={onChangeInput}
-            />
-            {inputTextarea.hasValue ? (
-              <ButtonBox>답변 완료</ButtonBox>
+            <InputTextarea type="답변" height="18.6rem" marginbottom="0.8rem" />
+            {inputTextarea ? (
+              <ButtonBox onClickBtnInput={handleClickButtonBox}>
+                답변 완료
+              </ButtonBox>
             ) : (
               <ButtonBox disabled>답변 완료</ButtonBox>
             )}
           </>
         )}
-        {answer ? (
+        {answer && editMode && (
+          <>
+            <InputTextarea
+              type="답변"
+              height="18.6rem"
+              marginbottom="0.8rem"
+              content={answer.content}
+            />
+            {inputTextarea ? (
+              <ButtonBox onClickBtnInput={handleClickButtonBox}>
+                수정 완료
+              </ButtonBox>
+            ) : (
+              <ButtonBox disabled>수정 완료</ButtonBox>
+            )}
+          </>
+        )}
+        {answer && !answer.isRejected && !editMode ? (
           <AnswerContent content={answer.content} />
         ) : (
           <AnswerContent content="" />
         )}
+        {answer && answer.isRejected && <AnswerRejected />}
       </FlexColumn>
     </Container>
   );
