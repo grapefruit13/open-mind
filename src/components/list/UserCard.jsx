f080d31e01fe1f769571fdcb6913168656b95cf9
DongKyuK12 <jkdk981024@gmail.com>
2023-12-20 00:27:11 +0900

    feat : 반응형 그리드에 맞게 너비,높이 값 수정, 데이터 불러왔을시 값만 표시되도록 테스트문자열 삭제

--- src/components/list/UserCard.jsx
+++ src/components/list/UserCard.jsx
@@ -2,17 +2,28 @@ import styled from 'styled-components';
 import Message from '../../assets/svgComponents/Message';
 
 const StyledUserCard = styled.div`
-  width: 22rem;
+  max-width: 22rem;
+  min-width: 18.6rem;
   height: 18.7rem;
   padding: 2rem;
   border-radius: 1.6rem;
   border: 1px solid var(--grayscale-40);
   background: var(--grayscale-10);
 
+  @media (max-width: 767px) {
+    height: 16.8rem;
+    padding: 1.6rem;
+  }
+
   img.user-profile {
     display: block;
     width: 6rem;
     height: 6rem;
+    border-radius: 50%;
+    @media (max-width: 767px) {
+      width: 4.8rem;
+      height: 4.8rem;
+    }
   }
 
   p.user-name {
@@ -21,6 +32,9 @@ const StyledUserCard = styled.div`
     font-family: Actor;
     font-size: 2rem;
     line-height: 125%;
+    @media (max-width: 767px) {
+      font-size: 1.8rem;
+    }
   }
 
   .received-questions-container {
@@ -35,14 +49,25 @@ const StyledUserCard = styled.div`
       width: 1.8rem;
       height: 1.8rem;
       margin-right: 0.4rem;
+
+      @media (max-width: 767px) {
+        width: 1.6rem;
+        height: 1.6rem;
+      }
     }
     p {
       font-size: 1.6rem;
+      @media (max-width: 767px) {
+        font-size: 1.4rem;
+      }
     }
     span {
       margin-right: 0;
       margin-left: auto;
-      font-size: 1.4rem;
+      font-size: 1.6rem;
+      @media (max-width: 767px) {
+        font-size: 1.4rem;
+      }
     }
   }
 `;
@@ -51,9 +76,9 @@ function UserCard({ imgSrc, userName, questions }) {
   return (
     <StyledUserCard>
       <img className="user-profile" src={imgSrc} alt="User" />
-      <p className="user-name">{userName}아초는 고양이</p>
+      <p className="user-name">{userName}</p>
       <div className="received-questions-container">
-        <Message />
+        <Message size="18" />
         <p>받은 질문</p>
         <span>{questions}개</span>
       </div>
